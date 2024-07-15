f177a6d649f432a7aa45f2f6d8125c011d3d2fc9
Karina <milkokarina13@gmail.com>
2024-07-15 12:43:29 +0300

    Add use state navlinkS

--- src/App.jsx
+++ src/App.jsx
@@ -91,50 +91,62 @@
 // export default App;
 //=============================================================
 
-import { useState } from "react";
-import CategoriesList from "./components/CategoriesList";
+// import { useState } from "react";
+// import CategoriesList from "./components/CategoriesList";
+// import NavLinks from "./components/NavLinks";
+
+// const NAV_LINK = {
+//   icon: "/icons/learn.svg",
+//   menuItem: "Навчання",
+//   link: "#",
+//   hasNotifications: false,
+//   isActive: false,
+// };
+
+// function App() {
+//   const [navLink, setNavLink] = useState(NAV_LINK);
+
+//   return (
+//     <>
+//       {/* <NavLinks /> */}
+//       <NavLink navLink={navLink} setNavLink={setNavLink} />
+//     </>
+//   );
+// }
+
+// export default App;
+
+// function NavLink({ navLink, setNavLink }) {
+//   const linkStyle = {
+//     border: navLink.isActive ? "5px solid blue" : "",
+//   };
+
+//   function changeActive() {
+//     // navLink.isActive = !navLink.isActive
+
+//     // STATE is IMMUTABLE
+//     const navLinkCopy = { ...navLink };
+//     navLinkCopy.isActive = !navLinkCopy.isActive;
+//     setNavLink(navLinkCopy);
+//   }
+
+//   return (
+//     <div key={navLink.icon}>
+//       <a onClick={changeActive} style={linkStyle} href={navLink.link}>
+//         <img src={navLink.icon} /> {navLink.menuItem}
+//       </a>
+//     </div>
+//   );
+// }
+//========================================================================================
 import NavLinks from "./components/NavLinks";
 
-const NAV_LINK = {
-  icon: "/icons/learn.svg",
-  menuItem: "Навчання",
-  link: "#",
-  hasNotifications: false,
-  isActive: false,
-};
-
 function App() {
-  const [navLink, setNavLink] = useState(NAV_LINK);
-
   return (
     <>
-      {/* <NavLinks /> */}
-      <NavLink navLink={navLink} setNavLink={setNavLink} />
+      <NavLinks />
     </>
   );
 }
 
 export default App;
-
-function NavLink({ navLink, setNavLink }) {
-  const linkStyle = {
-    border: navLink.isActive ? "5px solid blue" : "",
-  };
-
-  function changeActive() {
-    // navLink.isActive = !navLink.isActive
-
-    // STATE is IMMUTABLE
-    const navLinkCopy = { ...navLink };
-    navLinkCopy.isActive = !navLinkCopy.isActive;
-    setNavLink(navLinkCopy);
-  }
-
-  return (
-    <div key={navLink.icon}>
-      <a onClick={changeActive} style={linkStyle} href={navLink.link}>
-        <img src={navLink.icon} /> {navLink.menuItem}
-      </a>
-    </div>
-  );
-}
--- src/components/NavLinks/index.jsx
+++ src/components/NavLinks/index.jsx
@@ -1,88 +1,186 @@
-import classNames from "classnames";
+// import classNames from "classnames";
+// import styles from "./NavLinks.module.css";
+
+// function NavLinks() {
+//   const navLinks = [
+//     {
+//       icon: "/icons/learn.svg",
+//       menuItem: "Навчання",
+//       link: "#",
+//       hasNotifications: false,
+//       isActive: false,
+//     },
+//     {
+//       icon: "/icons/sounds.svg",
+//       menuItem: "Звуки",
+//       link: "#",
+//       hasNotifications: false,
+//       isActive: true,
+//     },
+//     {
+//       icon: "/icons/practice.svg",
+//       menuItem: "Практика",
+//       link: "#",
+//       hasNotifications: true,
+//       isActive: false,
+//     },
+//     {
+//       icon: "/icons/resultsBoard.svg",
+//       menuItem: "Дошки пошани",
+//       link: "#",
+//       hasNotifications: false,
+//       isActive: false,
+//     },
+//     {
+//       icon: "/icons/quests.svg",
+//       menuItem: "Квести",
+//       link: "#",
+//       hasNotifications: false,
+//       isActive: false,
+//     },
+//     {
+//       icon: "/icons/store.svg",
+//       menuItem: "Магазин",
+//       link: "#",
+//       hasNotifications: false,
+//       isActive: false,
+//     },
+//     {
+//       icon: "/icons/profile.png",
+//       menuItem: "Профіль",
+//       link: "#",
+//       hasNotifications: false,
+//       isActive: false,
+//     },
+//     {
+//       icon: "/icons/more.svg",
+//       menuItem: "Більше",
+//       link: "#",
+//       hasNotifications: false,
+//       isActive: false,
+//     },
+//   ];
+
+//   const mapNavList = (nl) => {
+//     // const linkClassName = `${styles.navLink}${
+//     //   nl.isActive ? ` ${styles.activeNavLink}` : ""
+//     // }`;
+//     // const linkClassName = `${styles.navLink}${
+//     //   nl.isActive ? ` ${styles.activeNavLink}` : ""
+//     // }`;
+
+//     const linkClassName = classNames(styles.navLink, {
+//       [styles.activeNavLink]: nl.isActive,
+//     });
+
+//     return (
+//       <li key={nl.icon}>
+//         <a className={linkClassName} href={nl.link}>
+//           <img className={styles.navImg} src={nl.icon} /> {nl.menuItem}
+//         </a>
+//       </li>
+//     );
+//   };
+
+//   return <ul className={styles.navList}>{navLinks.map(mapNavList)}</ul>;
+// }
+
+// export default NavLinks;
+//=======================================================================
+import { useState } from "react";
 import styles from "./NavLinks.module.css";
+import NavLink from "./NavLink";
+
+const NAV_LINKS = [
+  {
+    icon: "/icons/learn.svg",
+    menuItem: "Навчання",
+    link: "#",
+    hasNotifications: false,
+    isActive: false,
+  },
+  {
+    icon: "/icons/sounds.svg",
+    menuItem: "Звуки",
+    link: "#",
+    hasNotifications: false,
+    isActive: false,
+  },
+  {
+    icon: "/icons/practice.svg",
+    menuItem: "Практика",
+    link: "#",
+    hasNotifications: true,
+    isActive: false,
+  },
+  {
+    icon: "/icons/resultsBoard.svg",
+    menuItem: "Дошки пошани",
+    link: "#",
+    hasNotifications: false,
+    isActive: false,
+  },
+  {
+    icon: "/icons/quests.svg",
+    menuItem: "Квести",
+    link: "#",
+    hasNotifications: false,
+    isActive: false,
+  },
+  {
+    icon: "/icons/store.svg",
+    menuItem: "Магазин",
+    link: "#",
+    hasNotifications: false,
+    isActive: false,
+  },
+  {
+    icon: "/icons/profile.png",
+    menuItem: "Профіль",
+    link: "#",
+    hasNotifications: false,
+    isActive: false,
+  },
+  {
+    icon: "/icons/more.svg",
+    menuItem: "Більше",
+    link: "#",
+    hasNotifications: false,
+    isActive: false,
+  },
+];
 
 function NavLinks() {
-  const navLinks = [
-    {
-      icon: "/icons/learn.svg",
-      menuItem: "Навчання",
-      link: "#",
-      hasNotifications: false,
-      isActive: false,
-    },
-    {
-      icon: "/icons/sounds.svg",
-      menuItem: "Звуки",
-      link: "#",
-      hasNotifications: false,
-      isActive: true,
-    },
-    {
-      icon: "/icons/practice.svg",
-      menuItem: "Практика",
-      link: "#",
-      hasNotifications: true,
-      isActive: false,
-    },
-    {
-      icon: "/icons/resultsBoard.svg",
-      menuItem: "Дошки пошани",
-      link: "#",
-      hasNotifications: false,
-      isActive: false,
-    },
-    {
-      icon: "/icons/quests.svg",
-      menuItem: "Квести",
-      link: "#",
-      hasNotifications: false,
-      isActive: false,
-    },
-    {
-      icon: "/icons/store.svg",
-      menuItem: "Магазин",
-      link: "#",
-      hasNotifications: false,
-      isActive: false,
-    },
-    {
-      icon: "/icons/profile.png",
-      menuItem: "Профіль",
-      link: "#",
-      hasNotifications: false,
-      isActive: false,
-    },
-    {
-      icon: "/icons/more.svg",
-      menuItem: "Більше",
-      link: "#",
-      hasNotifications: false,
-      isActive: false,
-    },
-  ];
+  const [navLinks, setNavLinks] = useState(NAV_LINKS);
 
-  const mapNavList = (nl) => {
-    // const linkClassName = `${styles.navLink}${
-    //   nl.isActive ? ` ${styles.activeNavLink}` : ""
-    // }`;
-    // const linkClassName = `${styles.navLink}${
-    //   nl.isActive ? ` ${styles.activeNavLink}` : ""
-    // }`;
+  function changeActiveLink(selectedNavLinkIndex) {
+    const copyNavLinks = [...navLinks];
+    // Знайти індекс минулого виділеного
+    const prevActiveNavLinkIndex = copyNavLinks.findIndex(
+      (nlItem) => nlItem.isActive
+    );
 
-    const linkClassName = classNames(styles.navLink, {
-      [styles.activeNavLink]: nl.isActive,
-    });
+    // скидаємо для нього isActive
+    if (prevActiveNavLinkIndex !== -1) {
+      copyNavLinks[prevActiveNavLinkIndex].isActive = false;
+    }
 
+    // встановлюємо для нового виділеного isActive
+    copyNavLinks[selectedNavLinkIndex].isActive = true;
+    setNavLinks(copyNavLinks);
+  }
+
+  const mapNavList = (nl, index) => {
     return (
-      <li key={nl.icon}>
-        <a className={linkClassName} href={nl.link}>
-          <img className={styles.navImg} src={nl.icon} /> {nl.menuItem}
-        </a>
-      </li>
+      <NavLink
+        key={nl.icon}
+        navLink={nl}
+        linkIndex={index}
+        changeActiveLink={changeActiveLink}
+      />
     );
   };
 
   return <ul className={styles.navList}>{navLinks.map(mapNavList)}</ul>;
 }
-
 export default NavLinks;
