08b0cf38ad3ea995f25b6a36325f08fe99d984ef
Karina <milkokarina13@gmail.com>
2024-07-28 18:47:53 +0300

    Add data loading

--- src/components/UsersLoader/index.jsx
+++ src/components/UsersLoader/index.jsx
@@ -1,3 +1,46 @@
+// import { Component } from "react";
+
+// class UsersLoader extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       users: [],
+//       isFetching: false,
+//       error: null,
+//     };
+//   }
+//   //cdm
+//   componentDidMount() {
+//     console.log("mount");
+//   }
+
+//   //cdup
+//   componentDidUpdate(prevProps, prevState) {
+//     console.log("update");
+//   }
+
+//   //cwun
+//   componentWillUnmount() {
+//     console.log("unmount");
+//   }
+
+//   click = () => {
+//     console.log("setState");
+//     this.setState({ isFetching: !this.state.isFetching });
+//   };
+//   render() {
+//     console.log("render");
+//     return (
+//       <div>
+//         <button onClick={this.click}>+</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default UsersLoader;
+
 import { Component } from "react";
 
 class UsersLoader extends Component {
@@ -10,30 +53,24 @@ class UsersLoader extends Component {
       error: null,
     };
   }
-  //cdm
   componentDidMount() {
-    console.log("mount");
+    this.setState({ isFetching: true });
+    fetch(`https://randomuser.me/api?page=1&results=5&seed=js`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ users: data.results }))
+      .catch((err) => this.setState({ error: err }))
+      .finally(() => this.setState({ isFetching: false }));
   }
 
-  //cdup
-  componentDidUpdate(prevProps, prevState) {
-    console.log("update");
-  }
-
-  //cwun
-  componentWillUnmount() {
-    console.log("unmount");
-  }
-
-  click = () => {
-    console.log("setState");
-    this.setState({ isFetching: !this.state.isFetching });
-  };
   render() {
-    console.log("render");
+    const { users, isFetching, error } = this.state;
     return (
       <div>
-        <button onClick={this.click}>+</button>
+        {isFetching && <div>Loading... Please wait.</div>}
+        {error && <div>ERROR</div>}
+        {!isFetching &&
+          !error &&
+          users.map((u) => <li key={u.login.uuid}>{JSON.stringify(u)}</li>)}
       </div>
     );
   }
