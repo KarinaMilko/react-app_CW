8880cc0b86a5b74ef5d3f053c862d355dee8ffc1
Karina <milkokarina13@gmail.com>
2024-08-05 16:46:37 +0300

    Add form style

--- src/components/forms/ContactsForm/index.jsx
+++ src/components/forms/ContactsForm/index.jsx
@@ -1,5 +1,6 @@
-import { Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import { CONTACTS_VALIDATION_SCHEMA } from "../../../utils/validationSchemas";
+import styles from "./ContactsForm.module.sass";
 
 // Name, Tel, Email, Birthday
 // userName, phoneNumber, email, birthday
@@ -23,7 +24,64 @@ function ContactsForm() {
         onSubmit={handleSubmit}
         validationSchema={CONTACTS_VALIDATION_SCHEMA}
       >
-        {(formikProps) => <Form></Form>}
+        {(formikProps) => (
+          <Form className={styles.form}>
+            <label className={styles.label}>
+              <span className={styles.inputCaption}>Name: </span>
+              <Field
+                className={styles.input}
+                type="text"
+                name="userName"
+                placeholder="User Name"
+                autoFocus
+              />
+              <ErrorMessage
+                className={styles.error}
+                name="userName"
+                component="span"
+              />
+            </label>
+            <label className={styles.label}>
+              <span className={styles.inputCaption}>Phone: </span>
+              <Field
+                className={styles.input}
+                type="text"
+                name="phoneNumber"
+                placeholder="+380XXXXXXXXX"
+              />
+              <ErrorMessage
+                className={styles.error}
+                name="phoneNumber"
+                component="span"
+              />
+            </label>
+            <label className={styles.label}>
+              <span className={styles.inputCaption}>Email: </span>
+              <Field
+                className={styles.input}
+                type="email"
+                name="email"
+                placeholder="your@mail"
+              />
+              <ErrorMessage
+                className={styles.error}
+                name="email"
+                component="span"
+              />
+            </label>
+            <label className={styles.label}>
+              <span className={styles.inputCaption}>Date of Birth: </span>
+              <Field className={styles.input} type="date" name="birthday" />
+              <ErrorMessage
+                className={styles.error}
+                name="birthday"
+                component="span"
+              />
+            </label>
+            <button type="submit">Save</button>
+            <button type="reset">Reset</button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
